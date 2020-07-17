5c69db4ead94d1f0067ce45bbfb861e69d194b9f
AliceTrinta <atg200210@outlook.com>
2020-07-17 18:41:26 -0300

    Commands.go update

--- fundamentals/commands/commands.go
+++ fundamentals/commands/commands.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main()  {
 	//Testing go Commands...
-	//fmt.Printf("Another program in %s!")
-	//fmt.Printf("Another program in %s!", "Go")
+	//fmt.Printf("Another program in %s!") -- To see how go vet runs, uncomment this line.
+	fmt.Printf("Another program in %s!", "Go")
+
+	/*
+	Execute some programs in your terminal!
+	go env -- Gives environment information.
+	go version -- Shows the go version.
+	go vet file.go -- Get the line where there are errors.
+	go run file.go -- Compile and run a go file.
+	go build file.go -- Build an .exe of the file.
+	go get github.com.br/go-sql-driver -- Get some package from hubs of repositories.
+	.
+	.
+	.
+	*/
+
 }
